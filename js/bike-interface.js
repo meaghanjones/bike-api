2e622c01e0596f0b785f8c8b691188f612fe201e
Meaghan Jones and Matt Wilkin <meaghan@>
2016-08-03 14:27:48 -0700

    establish bike-counter functionality and result printing.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -4,14 +4,9 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   var currentBikeObject = new Bike();
-  $('#bikeColor').click(function(){
-    var color = $('#color').val();
-  $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&colors=' + color).then(function(response) {
-    response.bikes.forEach(function(bike) {
-      $("ul.showBikes").append("<li>" + bike.manufacturer_name + ", " + bike.stolen_location + "</li>");
-  }).fail(function(error){
-    $('.showBikes').text(error.responseJSON.message);
-  });
-});
+  $('#bikeCity').click(function(){
+    var city = $('#city').val();
+    var stolenBikes = currentBikeObject.getBikesByLocation(city);
+
 });
 });
--- js/bike.js
+++ js/bike.js
@@ -1,8 +1,40 @@
 function Bike() {
 }
 
-Bike.prototype.getBikesByColor = function() {
-  console.log("Hi! I'm a bike object");
+
+Bike.prototype.getBikesByLocation = function() {
+
+  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + city + '&proximity_square=100&stolen_after=1438452330').then(function(response) {
+    var trekCounter = 0;
+    var specializedCounter = 0;
+    var konaCounter = 0;
+    var cannondaleCounter = 0;
+    var giantCounter = 0;
+    var miscCounter = 0;
+    response.bikes.forEach(function(bike) {
+      if (bike.manufacturer_name === "Trek"){
+        trekCounter += 1;
+      } else if (bike.manufacturer_name === "Specialized") {
+        specializedCounter +=1;
+      } else if (bike.manufacturer_name === "Kona") {
+        konaCounter +=1;
+      } else if (bike.manufacturer_name === "Cannondale") {
+        cannondaleCounter +=1;
+      } else if (bike.manufacturer_name === "Giant") {
+        giantCounter +=1;
+      } else {
+        miscCounter +=1;
+      }
+    });
+  $("#showBikes").append("There were " + trekCounter + " Trek bikes stolen" + "." +"<br>"
+  + "There were " + specializedCounter + " Specialized bikes stolen" + ". " + "<br>"
+  + "There were " + konaCounter + " Kona bikes stolen" + ". " + "<br>"
+  + "There were " + cannondaleCounter + " Cannondale bikes stolen" + ". " + "<br>"
+  + "There were " + giantCounter + " Giant bikes stolen" + ". " + "<br>"
+  + "There were " + miscCounter + " other bikes stolen" + ". ");
+}).fail(function(error){
+      $('#showBikes').text(error.responseJSON.message);
+  });
 };
 
 exports.bikeModule = Bike;
