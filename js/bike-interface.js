e8f63e217d7a798585a4bcfc46791464392f8102
Meaghan Jones and Matt Wilkin <matt@>
2016-08-03 10:38:41 -0700

    fix issues in order to make it possible to print bikes of a certain color on the page

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
   var currentBikeObject = new Bike();
   $('#bikeColor').click(function(){
     var color = $('#color').val();
-  $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&colors=' + color, function(response) {
-    console.log(response);
+  $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&colors=' + color).then(function(response) {
+    response.bikes.forEach(function(bike) {
+      $("ul.showBikes").append("<li>" + bike.manufacturer_name + "</li>");
   }).fail(function(error){
     $('.showBikes').text(error.responseJSON.message);
   });
+});
 });
 });
